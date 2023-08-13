a55eb95570626bcb812c40ba669cf5413677da0f
Ganesh Maharna <ganeshmaharna@gmail.com>
2023-08-13 09:17:58 +0530

    This is for checking the posts by removing authenticateToken

--- routes/route.js
+++ routes/route.js
@@ -13,7 +13,8 @@ router.post('/login',loginUser);
 router.post('/file/upload', upload.single('file'), uploadImage);
 router.get('/file/:filename',getImage);
 router.post('/create',authenticateToken,createPost);
-router.get('/posts',authenticateToken,getAllPosts);
+// router.get('/posts',authenticateToken,getAllPosts);
+router.get('/posts',getAllPosts)
 router.get('/post/:id',authenticateToken,getPost);
 router.put('/update/:id',authenticateToken,updatePost);
 router.delete('/delete/:id',authenticateToken,deletePost);
